f70a7aee18e67a5a5cf9446d4ccacf17358e7cb5
Carlos Draper Giggs <blogcorriente@gmail.com>
2020-06-19 09:16:42 +0200

    Adds initial workflow elements

--- src/pages/workflow.js
+++ src/pages/workflow.js
@@ -4,13 +4,18 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import WorkflowElements from "../components/workflow-elements"
 
 const WorflowPage = () => (
   <Layout innerHeight="100%" outerHeight="60vh" backgroundColor="#F9BCBF" color="#3B625C" gridType="baseline">
     <SEO title="Workflow" />
     <div className="col-1"></div>
-    <div className="col-7"><h1 class="workflow-over-title font-rubik">Our workflow</h1></div>
+    <div className="col-7">
+      <h1 className="workflow-over-title font-rubik">Our workflow</h1>
+      <h1 className="workflow-title font-lust">It's all about process</h1>
+    </div>
     <div className="col-4"></div>
+    <WorkflowElements></WorkflowElements>
   </Layout>
 )
 
